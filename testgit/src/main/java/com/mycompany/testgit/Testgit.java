c4095bb9a99c2a85ef9d23032d869c1dde5fddb9
Lucas Garcia <lucas.garciadelacerda@gmail.com>
2024-09-19 13:38:02 -0300

    Criei o arquivo Index em 19/09/2024

---
+++ testgit/src/main/java/com/mycompany/testgit/Testgit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgit;
+
+/**
+ *
+ * @author Lucas Garcia <lucas.garciadelacerda@gmail.com>
+ */
+public class Testgit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ testgit/src/main/java/index/teste.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package index;
+
+/**
+ *
+ * @author Lucas Garcia <lucas.garciadelacerda@gmail.com>
+ * @date 19/09/2024
+ * @brief Class teste
+ */
+public class teste {
+
+}
